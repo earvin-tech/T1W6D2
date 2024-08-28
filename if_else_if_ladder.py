d2778c4c92e904b0952e9d2e4c22c557990fe08c
Earvin <wsl@Earvin>
2024-08-28 21:10:31 +1000

    Initial commit

---
+++ if_else_if_ladder.py
@@ -0,0 +1,20 @@
+# If else if ladder
+
+# >80 -> HD
+# 70-80 -> D
+# 60-70 -> C
+# 50-60 -> P
+# <50 -> F 
+
+marks = 61
+
+if marks >= 80:
+    print("HD")
+elif marks >= 70:
+    print("D")
+elif marks >= 60:
+    print("C")
+elif marks >= 50:
+    print("P")
+else:
+    print("F")
---
+++ ternery.py
@@ -0,0 +1,13 @@
+# Ternery
+
+is_raining = True
+
+if is_raining:
+    print("I need an umbrella")
+else:
+    print("I don't need an umbrella")
+
+# Truthy execution if condition else falsy execution
+print("I need an umbrella") if is_raining else print("I don't need an umbrella")
+
+print("The End!!")
